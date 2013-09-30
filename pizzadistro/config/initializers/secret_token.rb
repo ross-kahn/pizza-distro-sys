de87f68bf584d2988c68d2557e79c1954b3da4ce
Ross Kahn <ross.kahn@outlook.com>
2013-09-30 12:18:28 -0400

    Set up project skeleton
    Set up the project, and modified certain files to work with postgres. If
    you follow the tutorial on the main page, everyone should have the same
    operating environment and so can push here without affecting anyone
    else.

---
+++ pizzadistro/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pizzadistro::Application.config.secret_token = '0efb4b5497fe364f9e32fc38fd0a5854e846c7f0a1fac6b7714d4981d0a58e21e8082ce61aa9c854355767379764f76e453d9ff865aa0dd19591ee5ee86f42b3'
